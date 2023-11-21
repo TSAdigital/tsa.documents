c68c9af6d6c36e8d0c3850cc30de70f0a1a853df
Сергей <s.tarhanov174@gmail.com>
2023-11-21 18:57:18 +0500

    Version 1.1.6

--- views/auth-item/_search.php
+++ views/auth-item/_search.php
@@ -8,35 +8,21 @@ use yii\widgets\ActiveForm;
 /** @var yii\widgets\ActiveForm $form */
 ?>
 
-<div class="auth-item-search">
+<?php $form = ActiveForm::begin([
+    'action' => ['index'],
+    'method' => 'get',
+    'options' => [
+        'data-pjax' => 1
+    ],
+]); ?>
 
-    <?php $form = ActiveForm::begin([
-        'action' => ['index'],
-        'method' => 'get',
-        'options' => [
-            'data-pjax' => 1
-        ],
-    ]); ?>
+<?= $form->field($model, 'description') ?>
 
-    <?= $form->field($model, 'name') ?>
+<?= $form->field($model, 'status')->dropDownList($model->getStatusesArray(), ['prompt' => 'Выберите статус...']) ?>
 
-    <?= $form->field($model, 'type') ?>
-
-    <?= $form->field($model, 'description') ?>
-
-    <?= $form->field($model, 'rule_name') ?>
-
-    <?= $form->field($model, 'data') ?>
-
-    <?php // echo $form->field($model, 'created_at') ?>
-
-    <?php // echo $form->field($model, 'updated_at') ?>
-
-    <div class="form-group">
-        <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
-        <?= Html::resetButton('Reset', ['class' => 'btn btn-outline-secondary']) ?>
-    </div>
-
-    <?php ActiveForm::end(); ?>
-
+<div class="row">
+    <div class="col-8"><?= Html::submitButton('<i class="fas fa-search text-primary"></i>Поиск', ['class' => 'btn btn-app mx-auto btn-block mb-0']) ?></div>
+    <div class="col-4"><?= Html::a('<i class="fas fa-redo text-dark"></i>Сброс', ['index'], ['class' => 'btn btn-app mx-auto btn-block mb-0']) ?></div>
 </div>
+
+<?php ActiveForm::end(); ?>
