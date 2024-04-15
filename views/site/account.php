5dea3b1d952578f0946a1b9c9c00d6fd67e155de
Сергей <s.tarhanov174@gmail.com>
2024-04-15 22:05:22 +0500

    Version 1.4.8

--- views/site/account.php
+++ views/site/account.php
@@ -38,7 +38,7 @@ $this->params['breadcrumbs'][] = 'Личный кабинет';
                     <div class="card-header p-2" id="headingThree">
                         <h2 class="mb-0">
                             <button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                                Новости и объявления <span class="badge badge-danger text-bold"><?= ($tasks_count > 0) ? $tasks_count : false ?></span>
+                                Новости и объявления
                             </button>
                         </h2>
                     </div>
